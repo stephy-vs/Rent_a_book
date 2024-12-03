c79672bce05bc37099b4980bb61ea614d648ac9b
stephy-vs <vsstephy.ptf@gmail.com>
2024-12-03 22:29:58 +0530

    initial-commit-2024-12-03

---
+++ src/main/java/com/RentABook/RentABookApplication.java
@@ -0,0 +1,13 @@
+package com.RentABook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentABookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentABookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RentABook/RentABookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RentABook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentABookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
